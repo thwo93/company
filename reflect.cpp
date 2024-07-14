5f67372b2eb30adec72a4e8eb30767cae03a1a16
thwo93 <cos38zb@outlook.com>
2024-07-14 15:18:01 +0800

    Modify reflect.cpp

--- reflect.cpp
+++ reflect.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Especially author question take against play.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
