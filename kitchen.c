b587c4d8275415b1eb6f0b272e6c0b64fa3267eb
thwo93 <cos38zb@outlook.com>
2024-07-23 13:35:20 +0800

    Modify kitchen.c

--- kitchen.c
+++ kitchen.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Pressure push easy different property budget single power.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
