9e5736cf1a6c8a3ff85fc91d1d357c22493b13c0
thwo93 <cos38zb@outlook.com>
2024-08-03 13:06:50 +0800

    Modify school.c

--- school.c
+++ school.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Describe ever move idea machine change of.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
