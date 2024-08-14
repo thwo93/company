9b809bd61e4fce7daa359b72f8615f319c073400
thwo93 <cos38zb@outlook.com>
2024-08-14 09:11:46 +0800

    Modify tend.cpp

--- tend.cpp
+++ tend.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Space rule military near positive story.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
