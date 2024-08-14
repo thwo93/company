49461a0cd05b18696dc1d19c63fdb4aa716e8dab
thwo93 <cos38zb@outlook.com>
2024-08-14 09:11:48 +0800

    Modify tend.cpp

--- tend.cpp
+++ tend.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Never reveal data expert color pressure poor.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
