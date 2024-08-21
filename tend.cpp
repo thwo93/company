8f2dde8a3f54f289847595e60ef7ad0c9db759b5
thwo93 <cos38zb@outlook.com>
2024-08-21 20:00:21 +0800

    Modify tend.cpp

--- tend.cpp
+++ tend.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Very join with new.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
