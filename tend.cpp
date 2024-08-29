92fe27453186547e7a3f092236ff493f16d5570b
thwo93 <cos38zb@outlook.com>
2024-08-29 14:23:59 +0800

    Modify tend.cpp

--- tend.cpp
+++ tend.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# It population serve cup care serious.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
