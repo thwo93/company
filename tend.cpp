c6417d2b454b8137f57b98107d70ba537f6c3012
thwo93 <cos38zb@outlook.com>
2024-09-04 13:49:04 +0800

    Modify tend.cpp

--- tend.cpp
+++ tend.cpp
@@ -45,3 +45,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# He occur structure task cut voice paper.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
