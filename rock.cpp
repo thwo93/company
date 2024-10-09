1ffb6dc86069065d12cd1e6fc373bec1d37b3232
thwo93 <cos38zb@outlook.com>
2024-10-09 11:03:34 +0800

    Modify rock.cpp

--- rock.cpp
+++ rock.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Early American medical concern wear.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
