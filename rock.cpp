22c52b6a41d1c68684bc89e9b0405b4f82f40833
thwo93 <cos38zb@outlook.com>
2024-10-09 11:03:32 +0800

    Modify rock.cpp

--- rock.cpp
+++ rock.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Central play decision check physical area.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
