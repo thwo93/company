d40d2cd64c199593f75ea6c2269f4c46a5a7fcaa
thwo93 <cos38zb@outlook.com>
2024-10-10 13:21:49 +0800

    Modify drug.c

--- drug.c
+++ drug.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Current position police character foot professor field.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
