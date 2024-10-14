db776690906fd9b8a2b5653d0e0a31c2cd67d867
thwo93 <cos38zb@outlook.com>
2024-10-14 10:41:01 +0800

    Modify size.c

--- size.c
+++ size.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Enough consider number people from bar answer.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
