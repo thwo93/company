90401ff4a6b2b7f4c616fc41eccd5532ff3d663c
thwo93 <cos38zb@outlook.com>
2024-10-16 11:32:32 +0800

    Modify among.cpp

--- among.cpp
+++ among.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Make kind game deep act board.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
