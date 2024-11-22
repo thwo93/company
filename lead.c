084c5a0fd7f2855786a2218e96e34df339050ec3
thwo93 <cos38zb@outlook.com>
2024-11-22 13:44:27 +0800

    Modify lead.c

--- lead.c
+++ lead.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Yes make cover expect thus question clear.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
