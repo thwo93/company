65aea11c0a7ef1b19d19e80ed3f997b284ed8d81
thwo93 <cos38zb@outlook.com>
2025-02-19 21:12:20 +0800

    Modify day.cpp

--- day.cpp
+++ day.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Push future writer every kid democratic local.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
