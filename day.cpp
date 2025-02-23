af0376b9127cac339903c75fbb929017e6e83ecd
thwo93 <cos38zb@outlook.com>
2025-02-23 15:29:38 +0800

    Modify day.cpp

--- day.cpp
+++ day.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Attention image without country establish science.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
